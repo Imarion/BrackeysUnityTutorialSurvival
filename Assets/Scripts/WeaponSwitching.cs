760a4e2d59f684672051839b0cd3daead80b6f69
emr <renquin@awtce.be>
2019-05-13 08:24:14 +0200

    Episode 11: Weapon switching

---
+++ Assets/Scripts/WeaponSwitching.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour
+{
+    public GameObject Weapon01, Weapon02;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            SwapWeapons();
+        }
+    }
+
+    void SwapWeapons() {
+        if (Weapon01.activeSelf == true)
+        {
+            Weapon01.SetActive(false);
+            Weapon02.SetActive(true);
+        }
+        else {
+            Weapon01.SetActive(true);
+            Weapon02.SetActive(false);
+        }
+    }
+}
