5621aa5a647bcf56f560c21297df6ac4edfd65ee
emr <renquin@awtce.be>
2019-05-14 08:00:32 +0200

    Episode 13: audio zones.

---
+++ Assets/Scripts/AudioZones.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioZones : MonoBehaviour
+{
+    public AudioSource thisAudioSource;
+
+    private string TheCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        TheCollider = other.tag;
+        if (TheCollider == "Player") {
+            thisAudioSource.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TheCollider = other.tag;
+        if (TheCollider == "Player")
+        {
+            thisAudioSource.Stop();
+        }
+
+    }
+}
