97d3605548c2ac7a568e1f203d4d030dafd2e6ec
emr <renquin@awtce.be>
2019-05-16 07:47:56 +0200

    Episode 15: advanced ai

---
+++ Assets/Scripts/AdvancedAI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedAI : MonoBehaviour
+{
+    public float Distance;
+    public Transform Target;
+    public float lookAtDistance = 25.0f;
+    public float chaseRange = 15.0f;
+    public float attackRange = 1.5f;
+    public float moveSpeed = 2.0f;
+    public float Damping = 6.0f;
+    public float attackRepeatTime = 1.0f;    
+
+    public CharacterController controller;
+    public float gravity = 20.0f;
+
+    private MeshRenderer mr;
+    private Vector3 MoveDirection = Vector3.zero;
+
+    private float attackTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        attackTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance = Vector3.Distance(Target.position, transform.position);
+        if (Distance < lookAtDistance)
+        {            
+            lookAt();
+        }
+
+        if (Distance > lookAtDistance)
+        {
+            mr.material.color = Color.green;
+        }
+
+        if (Distance < attackRange) {
+            attack();
+        }
+        else if (Distance < chaseRange) {            
+            chase();
+        }
+    }
+
+    private void lookAt()
+    {
+        mr.material.color = Color.yellow;
+        Quaternion rotation = Quaternion.LookRotation(Target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * Damping);
+    }
+
+    private void chase()
+    {
+        mr.material.color = Color.red;
+        
+        //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        Vector3 moveDirection = transform.forward;
+        moveDirection *= moveSpeed;
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void attack() {
+        if (Time.time > attackTime)
+        {
+            Debug.Log("attacking");
+            attackTime = Time.time + attackRepeatTime;
+        }
+    }
+
+    public void ApplyDamage(int TheDamage) {
+        // enrage the ennemy
+        chaseRange += 30.0f;
+        moveSpeed += 2.0f;
+        lookAtDistance += 40.0f;
+    }
+}
