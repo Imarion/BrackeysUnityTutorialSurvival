0f77c0d12c389c169f72bbd192c737203feefc76
emr <renquin@awtce.be>
2019-05-22 14:00:30 +0200

    Episode 20: improved controls.

---
+++ Assets/Scripts/CrouchHeight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CrouchHeight : MonoBehaviour
+{
+    private CharacterController charController;
+    private Transform theTransform;
+    private float charHeight; //Initial height
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theTransform = transform;
+        charController = GetComponent<CharacterController>();
+        charHeight = charController.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = charHeight;
+
+        if (Input.GetKey("c"))
+        {
+            h = charHeight * 0.5f;
+        }
+
+        var lastHeight = charController.height; //Stand up/crouch smoothly
+        charController.height = Mathf.Lerp(charController.height, h, 5 * Time.deltaTime);
+        theTransform.position += new Vector3 (0.0f, (charController.height - lastHeight) / 2f, 0.0f); //Fix vertical position
+    }
+}
