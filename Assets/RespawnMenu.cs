3c4f9be92cf0e212507c1bbb0d62c9152a94671e
emr <renquin@awtce.be>
2019-05-22 08:47:05 +0200

    Ep19: buttons working (had to set cursor to visibile in the RespawnMenu script).

--- Assets/RespawnMenu.cs
+++ Assets/RespawnMenu.cs
@@ -3,44 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
+
 public class RespawnMenu : MonoBehaviour
 {
     public FirstPersonController fpc;
+    public GameObject MenuPanel;
 
     public static bool playerIsDead = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (playerIsDead == true) {
-            //fpc.enabled = false;
+            fpc.enabled = false;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+
+            MenuPanel.SetActive(true);
         }
     }
 
-    private void OnGUI()
-    {
-        if (playerIsDead == true)
-        {
-            if (GUI.Button(new Rect(Screen.width * 0.5f - 50, 200 - 20, 100, 40), "Restart"))
-            {
-                RespawnPlayer();
-            }
-
-            if (GUI.Button(new Rect(Screen.width * 0.5f - 50, 240, 100, 40), "Menu"))
-            {
-                Debug.Log("Return to Menu");
-            }
-        }
-    }
-
-    private void RespawnPlayer()
+    public void RespawnPlayer()
     {
         Debug.Log("RespawnPlayer");
+    }
+
+    public void GoToMainMenu()
+    {
+        Debug.Log("Return to Menu");
     }
 }
